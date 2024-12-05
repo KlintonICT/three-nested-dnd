9c81b6dd84918326736a7cde731cc1f769c4562c
KlintonICT <chhunklinton91@gmail.com>
2024-12-05 18:02:25 +0700

    refactor: comment unused component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // import MultipleContainers from "./MultipleContainer";
 import MyDND from "./MyDND";
-import ThreeNestedDND from "./ThreeNestedDnd";
+// import ThreeNestedDND from "./ThreeNestedDnd";
 
 const App = () => {
   return (
     <div className="bg-[#21265F]">
       <MyDND />
       {/* <MultipleContainers /> */}
-      <ThreeNestedDND />
+      {/* <ThreeNestedDND /> */}
     </div>
   );
 };
