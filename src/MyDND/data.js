a5af989c234210befdea1edd1f09aacda575033c
KlintonICT <chhunklinton91@gmail.com>
2024-12-06 12:12:33 +0700

    feat: update render data

--- src/MyDND/data.js
+++ src/MyDND/data.js
@@ -1,10 +1,10 @@
 export const initData = [
   {
     id: 'single',
-    name: 'Single Activities',
+    name: 'Single Process',
     activities: [
-      { id: 'a1', name: 'Activity 1' },
-      { id: 'a11', name: 'Activity 11' },
+      { id: 'A1', name: 'Activity 1' },
+      { id: 'A2', name: 'Activity 2' },
     ],
     subs: [],
   },
@@ -12,22 +12,22 @@ export const initData = [
     id: 'p2',
     name: 'Process 2',
     activities: [
-      { id: 'a2', name: 'Activity 2' },
-      { id: 'a22', name: 'Activity 22' },
+      { id: 'A3', name: 'Activity 3' },
+      { id: 'A4', name: 'Activity 4' },
     ],
     subs: [
       {
         id: "s1",
-        name: "Subprocess 1",
+        name: "Sub Process 1",
         activities: [
-          { id: "a3", name: "Activity 3" }
+          { id: "A5", name: "Activity 5" }
         ],
       },
       {
         id: "s2",
-        name: "Subprocess 2",
+        name: "Sub Process 2",
         activities: [
-          { id: "a4", name: "Activity 4" }
+          { id: "A6", name: "Activity 6" }
         ],
       },
     ],
@@ -36,23 +36,23 @@ export const initData = [
     id: 'p3',
     name: 'Process 3',
     activities: [
-      { id: "a5", name: "Activity 5" }
+      { id: "A7", name: "Activity 7" }
     ],
     subs: [
       {
         id: 's3',
-        name: 'Subprocess 3',
+        name: 'Sub Process 3',
         activities: [
-          { id: "a6", name: "Activity 6" },
-          { id: "a61", name: "Activity 61" },
+          { id: "A8", name: "Activity 8" },
+          { id: "A9", name: "Activity 9" },
         ],
       },
       {
         id: 's4',
-        name: 'Subprocess 4',
+        name: 'Sub Process 4',
         activities: [
-          { id: 'a7', name: 'Activity 7' },
-          { id: 'a8', name: 'Activity 8' },
+          { id: 'A10', name: 'Activity 10' },
+          { id: 'A11', name: 'Activity 11' },
         ],
       },
     ],
